fa935356d7e8155a12a0de9d434a77ed939dd5ab
BogdanSadolinskiy <nikebodya71@gmail.com>
2021-04-26 14:08:16 +0300

    initial spring boot and integration

---
+++ src/test/java/com/bodya/spring_integration_example/SpringIntegrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodya.spring_integration_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
